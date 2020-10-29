d3ad2a6250ecb0d2c16db23590a2cac5c58650e3
bdtmnk <leonid.didukh@desy.de>
2020-10-29 17:32:51 +0100

    Added list.sh year and type parametric datasets list creator

--- NTupleMaker/test/list.sh
+++ NTupleMaker/test/list.sh
@@ -1,55 +1,81 @@
 
 dir=$1
+year=$2
+tp=$3
 
-rm datasets${dir}
+#rm datasets${dir}
 #sources="/nfs/dust/cms/user/rasp/storage/76x_JECv2_MVAMET0p6/ /nfs/dust/cms/group/susy-desy/Run2/Stau/MC/25ns/76x_JECv2_MVAMET0p6/"
-sources="/nfs/dust/cms/user/rasp/storage/76x_JECv2_MVAMET0p6/"
+#sources="/nfs/dust/cms/user/bobovnii/CMSSW_8_0_20/src/DesyTauAnalyses/NTupleMaker/NtupleForWmnu"
+#sources="/nfs/dust/cms/user/rasp/storage/SingleElectron_Run2016-23Sep2016/"
+sources="/pnfs/desy.de/cms/tier2/store/user/ibabouni/higgs-kit/MC2017/"
+sources="/pnfs/desy.de/cms/tier2/store/user/telenz/13TeV/NTuples/MC/RunIIFall17MiniAOD-94X_mc2017_realistic/"
+sources="/nfs/dust/cms/user/rasp/ntuples/MC_2017"
+sources="/nfs/dust/cms/group/higgs-kit/rasp/MC_2017"
+sources="/nfs/dust/cms/user/rasp/ntuples/MC2017_newMET"
+sources="/nfs/dust/cms/user/mameyer/SM_HiggsTauTau/ntuples/METRecipev2/"
+sources="/nfs/dust/cms/user/tlenz/13TeV/2017/NTuples/MC/12Apr2018_PU2017_METrecipe_v2"
+sources="/nfs/dust/cms/user/rasp/ntuples/Data2017_newMET_v2/"
+sources="/nfs/dust/cms/user/tlenz/13TeV/2017/NTuples/MC/12Apr2018_PU2017_METrecipe_v2/"
+sources="/nfs/dust/cms/user/mameyer/SM_HiggsTauTau/ntuples/METRecipev2/"
 
-source2=/nfs/dust/cms/user/rasp/storage/76x_JECv2_MVAMET0p6_DatawFilters/
+#MC TauId Feb 2019:
+#sources="/nfs/dust/cms/user/cardinia/gridjobs/2018/NTuples/MC/RunIIAutumn18/"
+#sources="/nfs/dust/cms/user/ywen/Storage/MC/"
+#sources="/nfs/dust/cms/user/cardinia/gridjobs/2018/NTuples/MC/RunIIAutumn18"
+#sources="/nfs/dust/cms/user/ywen/Storage/SingleMuon/"
+#sources="/nfs/dust/cms/group/higgs-kit/2018/MC_RunIIAutumn18MiniAOD/"
+#sources="/nfs/dust/cms/group/higgs-kit/2018/MC_RunIIAutumn18MiniAOD/"
+#sources="/nfs/dust/cms/user/cardinia/gridjobs/2018/NTuples/MC/RunIIAutumn18/"
+
+#2016
+if [[ ${year} == "2016" ]]  &&   [[ ${tp} == "data" ]]; then
+sources="/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2016/data/"
+fi
+
+if [[ ${year} == "2016" ]]  && [[ ${tp} == "mc" ]]; then
+sources="/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2016/mc/"
+fi
+
+#2017
+
+if [[ ${year} == "2017" ]]  &&  [[ ${tp} == "data" ]]; then
+sources="/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2018/data/" 
+sources="/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/data/SingleMuon/"
+fi
+
+if [[ ${year} == "2017" ]]  &&  [[ ${tp} == "mc" ]]; then
+#sources="/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v3/" 
+sources="/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v4/" 
+#sources="/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2017/mc_v3/QCD/"
+fi
+
+#2018
+if [[ ${year} == "2018" ]]  &&  [[ ${tp} == "data" ]]; then
+sources="/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2018/data/SingleMuon/" 
+sources="/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2018/data/"
+fi
+
+if [[ ${year} == "2018" ]]  &&  [[ ${tp} == "mc" ]]; then
+sources="/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2018/mc/"
+sources="/pnfs/desy.de/cms/tier2/store/user/ldidukh/Missed_MC_v4/"
+#sources="/nfs/dust/cms/user/dydukhle/WenuDatasets/"
+sources="/pnfs/desy.de/cms/tier2/store/user/ldidukh/QCD_EM_Enriched_v2/"
+#sources="/pnfs/desy.de/cms/tier2/store/user/ldidukh/Missed_MC_v2/"
+fi
 
 alias ls='ls'
 
-#for i in `ls $source/`
 
 for source in $sources
 do
 for i in `ls $source/`
 do
-	ls $source/$i/*.root > ${dir}/$i
-	echo $i > $i
-	echo $i >> datasets${dir}
-	
+	ls $source/$i/*.root > ${dir}/${year}/${tp}/$i
+	echo $i >> list_${dir}_${year}_${tp}
+	echo hadd $i.root ${i}_*.root >> ${dir}/${year}/${tp}/merg.sh
+	echo rm ${i}_* >> ${dir}/${year}/${tp}/delete.sh
+	echo "" >> ${dir}/${year}/${tp}/merg.sh
 
-	echo hadd $i.root ${i}_*.root >> ${dir}/merg.sh
-	echo rm ${i}_* >> ${dir}/merg.sh
-	echo "" >> ${dir}/merg.sh
 done
 done
 
-
-ls $source2/SingleMuon*Run2015D*/*.root > ${dir}/SingleMuon
-ls $source2/SingleEl*Run2015D*/*.root > ${dir}/SingleElectron
-ls $source2/MuonEG*Run2015D*/*.root > ${dir}/MuonEG
-#ls $source/METw*/*.root > ${dir}/MET
-#echo MET > MET
-echo SingleMuon  > SingleMuon
-echo SingleElectron > SingleElectron
-echo MuonEG > MuonEG
-
-rm GC*
-rm ${dir}/GC*
-
-rm *Glu*
-rm $dir/*Glu*
-
-rm AToZh*
-rm $dir/AToZh*
-
-rm Charged*
-rm $dir/Charged*
-
-
-sed -i '/AToZh/d' datasets$dir
-sed -i '/Charged/d' datasets$dir
-sed -i '/Glu/d' datasets$dir
-sed -i '/GC/d' datasets$dir
